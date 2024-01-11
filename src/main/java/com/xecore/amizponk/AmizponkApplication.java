e747ec4d3feae553a80ceb807d30c176211710f6
NiCat Ahmedov <62359943+NedNedov228@users.noreply.github.com>
2024-01-12 00:56:37 +0400

    Added grandle src

---
+++ src/main/java/com/xecore/amizponk/AmizponkApplication.java
@@ -0,0 +1,13 @@
+package com.xecore.amizponk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmizponkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmizponkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xecore/amizponk/AmizponkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xecore.amizponk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmizponkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
